cce97a09329ef83a692cd2b19fef9020f8314867
Lucas Felix <lfelixsampaio@gmail.com>
2017-08-03 15:25:30 -0300

    Estrutura inicial da aplicacao

---
+++ src/components/Root/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Root extends Component {
+    render() {
+        return (
+            <div>
+                Entrada da Aplicaçâo
+            </div>
+        )
+    }
+}
+
+export default Root
