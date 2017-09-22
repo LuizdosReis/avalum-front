f4654fe892a3db6134ab9a535d35031413dd1cad
ThiagoAndrad <thpandrade@gmail.com>
2017-09-22 13:03:46 -0300

    [Redux]ajustando indentacao

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,43 +1,78 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Dia from '../../components/Dia/index'
 import Modal from '../../components/Modal/index'
 import PropTypes from 'prop-types'
 import './Dashboard.css'
-import { DashboardService } from "../../services/DashboardService"
+import {DashboardService} from "../../services/DashboardService"
 
 class Dashboard extends Component {
 
-  constructor () {
+  constructor() {
     super()
     this.state = {
       modal: {},
       dias: []
     }
 
-    this.toggleModalPergunta = this.toggleModalPergunta.bind(this)
     this.submitFormulario = this.submitFormulario.bind(this)
     this.setEnunciado = this.setEnunciado.bind(this)
     this.setTipoResposta = this.setTipoResposta.bind(this)
     this.setPeriodo = this.setPeriodo.bind(this)
     this.setResposta = this.setResposta.bind(this)
     this.adicionaNovoCampoDeResposta = this.adicionaNovoCampoDeResposta.bind(this)
+    this.toggleModalPergunta = this.toggleModalPergunta.bind(this)
     this.exibePergunta = this.exibePergunta.bind(this)
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.context.store.dispatch(DashboardService.listaDias())
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.context.store.subscribe(() => {
       this.setState(this.context.store.getState())
     })
   }
 
+  submitFormulario(event) {
+    event.preventDefault();
+    this.context.store.dispatch(DashboardService.salva(this.state.pergunta, this.state.dias))
+  }
+
+  setEnunciado(event) {
+    let pergunta = this.state.pergunta
+    pergunta.enunciado = event.target.value
+    this.setState({pergunta})
+  }
+
+  setTipoResposta(event) {
+    let pergunta = this.state.pergunta
+    pergunta.tipoResposta = event.target.value
+    this.setState({pergunta})
+  }
+
+  setPeriodo(event) {
+    let pergunta = this.state.pergunta
+    pergunta.periodo = event.target.value
+    this.setState({pergunta})
+  }
+
+  setResposta(event, index) {
+    let pergunta = this.state.pergunta
+    pergunta.respostas[index] = event.target.value
+    this.setState({pergunta})
+  }
+
+  adicionaNovoCampoDeResposta() {
+    let pergunta = this.state.pergunta
+    pergunta.respostas.push("")
+    this.setState({pergunta})
+  }
+
   toggleModalPergunta(event, dia) {
     event.stopPropagation()
 
-    if(event.target.classList.contains('jsModal__toggle')) {
+    if (event.target.classList.contains('jsModal__toggle')) {
       this.setState({
         modal: {
           visivel: !this.state.modal.visivel
@@ -52,44 +87,7 @@ class Dashboard extends Component {
     }
   }
 
-  submitFormulario(event) {
-    event.preventDefault();
-    this.context.store.dispatch(DashboardService.salva(this.state.pergunta, this.state.dias))
-  }
-
-
-  setEnunciado(event){
-    let pergunta = this.state.pergunta
-    pergunta.enunciado = event.target.value
-    this.setState({ pergunta })
-  }
-
-  setTipoResposta(event) {
-    let pergunta = this.state.pergunta
-    pergunta.tipoResposta = event.target.value
-    this.setState({ pergunta })
-  }
-
-  setPeriodo(event) {
-    let pergunta = this.state.pergunta
-    pergunta.periodo = event.target.value
-    this.setState({ pergunta })
-  }
-
-  setResposta(event, index) {
-    let pergunta = this.state.pergunta
-    pergunta.respostas[index] = event.target.value
-    this.setState({ pergunta })
-  }
-
-  adicionaNovoCampoDeResposta() {
-    let pergunta = this.state.pergunta
-    pergunta.respostas.push("")
-    this.setState({ pergunta })
-  }
-
-
-  exibePergunta(pergunta){
+  exibePergunta(pergunta) {
     this.setState({
       modal: {
         visivel: !this.state.modal.visivel
@@ -98,43 +96,38 @@ class Dashboard extends Component {
     })
   }
 
-  render () {
+  render() {
     return (
       <main className='pageDashboard'>
-
-
         {
-          this.state.dias.map( (dia) => {
-                      return (
-                        <Dia
-                             key={dia.numeroDia}
-                             dia={dia}
-                             mostraModalPergunta={ (event) => this.toggleModalPergunta(event, dia.numeroDia) }
-                             exibePergunta={this.exibePergunta}
-                             />
-                      )
-
-
-                    })
-
-
+          this.state.dias.map((dia) => {
+            return (
+              <Dia
+                key={dia.numeroDia}
+                dia={dia}
+                mostraModalPergunta={(event) => this.toggleModalPergunta(event, dia.numeroDia)}
+                exibePergunta={this.exibePergunta}
+              />
+            )
+          })
         }
 
-        { this.state.modal.visivel && <Modal item={ this.state.modal }
-                                             action={ this.submitFormulario }
-                                             setEnunciado={this.setEnunciado}
-                                             pergunta={ this.state.pergunta } fechaModal={ this.toggleModalPergunta }
-                                             setTipoResposta={ this.setTipoResposta }
-                                             setResposta={ this.setResposta }
-                                             setPeriodo={ this.setPeriodo }
-                                             adicionaNovoCampoDeResposta={ this.adicionaNovoCampoDeResposta }/> }
+        {this.state.modal.visivel && <Modal item={this.state.modal}
+                                            action={this.submitFormulario}
+                                            setEnunciado={this.setEnunciado}
+                                            pergunta={this.state.pergunta}
+                                            fechaModal={this.toggleModalPergunta}
+                                            setTipoResposta={this.setTipoResposta}
+                                            setResposta={this.setResposta}
+                                            setPeriodo={this.setPeriodo}
+                                            adicionaNovoCampoDeResposta={this.adicionaNovoCampoDeResposta}/>}
       </main>
     )
   }
 }
 
 Dashboard.contextTypes = {
-  store : PropTypes.object.isRequired
+  store: PropTypes.object.isRequired
 }
 
 export default Dashboard
