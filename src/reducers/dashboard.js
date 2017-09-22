404a3a48f521a9d3efb47e15e43d709b1005b5f2
ThiagoAndrad <thpandrade@gmail.com>
2017-09-22 16:00:06 -0300

    [Redux] trocando os ifs pelo switch case

--- src/reducers/dashboard.js
+++ src/reducers/dashboard.js
@@ -4,42 +4,43 @@ const initialState = {
 }
 
 export function dashboard(state = initialState, action) {
-  if (action.type === 'LISTAR_DIAS') {
-    state.dias = action.dias
-    return state
+
+  switch (action.type) {
+
+    case 'LISTAR_DIAS':
+      state.dias = action.dias
+      return state
+
+    case 'SALVAR_PERGUNTA':
+      let dia = state.dias.find(dia => dia.numeroDia === action.pergunta.dia);
+      if (dia.periodoPerguntas[action.pergunta.periodo])
+        dia.periodoPerguntas[action.pergunta.periodo].push(action.pergunta)
+      else {
+        let periodo = action.pergunta.periodo
+        dia.periodoPerguntas[periodo] = [action.pergunta]
+      }
+      state.modal.visivel = !state.modal.visivel
+      return state
+
+    case 'EXIBIR_PERGUNTA':
+      state.modal.visivel = !state.modal.visivel
+      state.modal.pergunta = action.pergunta
+      return state
+
+    case 'TOOGLE_MODAL_PERGUNTA':
+      state.modal.visivel = !state.modal.visivel
+      state.modal.pergunta = {
+        enunciado: "",
+        dia: action.dia,
+        tipoResposta: "",
+        respostas: [],
+        periodo: ""
+      }
+      return state
+
+    default:
+      return state
+
   }
 
-  if (action.type === 'SALVAR_PERGUNTA') {
-    let dia = state.dias.find(dia => dia.numeroDia === action.pergunta.dia);
-    if (dia.periodoPerguntas[action.pergunta.periodo])
-      dia.periodoPerguntas[action.pergunta.periodo].push(action.pergunta)
-    else {
-      let periodo = action.pergunta.periodo
-      dia.periodoPerguntas[periodo] = [action.pergunta]
-    }
-    state.modal.visivel = !state.modal.visivel
-    return state
-  }
-
-  if (action.type === 'EXIBIR_PERGUNTA') {
-    state.modal.visivel = !state.modal.visivel
-    state.modal.pergunta = action.pergunta
-
-    return state
-  }
-
-  if (action.type === 'TOOGLE_MODAL_PERGUNTA') {
-    state.modal.visivel = !state.modal.visivel
-    state.modal.pergunta = {
-      enunciado: "",
-      dia: action.dia,
-      tipoResposta: "",
-      respostas: [],
-      periodo: ""
-    }
-
-    return state
-  }
-
-  return state
 }
